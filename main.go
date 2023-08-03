4b3c4e073fc49b6c557a8c3bb32fd15511c3c7d1
VinayakBagaria <workvinayak95@gmail.com>
2023-08-03 21:11:16 +0530

    Initialize swagger

--- main.go
+++ main.go
@@ -10,15 +10,32 @@ import (
 	"github.com/VinayakBagaria/go-cat-pictures/api/routes"
 	"github.com/VinayakBagaria/go-cat-pictures/config"
 	"github.com/VinayakBagaria/go-cat-pictures/db"
+	"github.com/VinayakBagaria/go-cat-pictures/docs"
 	"github.com/VinayakBagaria/go-cat-pictures/repository"
 	"github.com/VinayakBagaria/go-cat-pictures/service"
 	"github.com/VinayakBagaria/go-cat-pictures/storage"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
 	config.Init()
 
+	router := gin.Default()
+	// Logger middleware will write the logs to gin.DefaultWriter = os.Stdout
+	router.Use(gin.Logger())
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	router.Use(gin.Recovery())
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.Static("/get-image", "./images")
+
+	// Set swagger data
+	docs.SwaggerInfo.Title = "Cat Pictures"
+	docs.SwaggerInfo.Description = "RESTful API for uploading and managing cat pictures."
+	docs.SwaggerInfo.Schemes = []string{"http"}
+
+	// Initialize the db
 	dbConfig := db.NewConfiguration()
 	dbHandler, err := db.NewConnection(dbConfig)
 	if err != nil {
@@ -34,16 +51,9 @@ func main() {
 	serverHandler := resthandlers.NewServerHandler()
 	serverRoutesList := routes.NewServerRouteList(serverHandler)
 
-	router := gin.Default()
-	// Logger middleware will write the logs to gin.DefaultWriter = os.Stdout
-	router.Use(gin.Logger())
-	// Recovery middleware recovers from any panics and writes a 500 if there was one.
-	router.Use(gin.Recovery())
-	router.MaxMultipartMemory = 8 << 20 // 8 MiB
-	router.Static("/get-image", "./images")
-
 	routes.Install(router, routesList)
 	routes.Install(router, serverRoutesList)
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	apiPort, err := strconv.Atoi(config.GetConfigValue("server.port"))
 	if err != nil {
